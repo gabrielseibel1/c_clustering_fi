3985d5bc33a872917de6f6d255354ec098a5cc2d
GerVeit <geronimoveit@gmail.com>
2018-04-20 17:11:13 -0300

    Agnes configuration

---
+++ src/AGNES/watchdog-AGNES.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import os
+import time
+import sys
+
+confFile = "/home/geronimo/Documents/FTF/carol-fi/codes/AGNES/agnes.conf"
+
+timestampFile = "summary-carolfi.log"
+timestampMaxDiff=60*5 # in seconds
+
+def killall():
+    os.system("killall -9 fault_injector.py")
+    os.system("killall -9 gdb")
+
+def run():
+    killall()
+    os.system("./fault_injector.py -c "+confFile+" -i 9000 &")
+
+try:
+    print "running ..."
+    run()
+    time.sleep(timestampMaxDiff)
+    while True:
+        timestamp = int(os.path.getmtime(timestampFile))
+        now = int(time.time())
+        timestampDiff = now - timestamp
+        if timestampDiff > timestampMaxDiff:
+            print "timestamp > than expected"
+            run()
+        else:
+            print "timestamp OK"
+        
+        time.sleep(timestampMaxDiff)
+
+except KeyboardInterrupt:  # Ctrl+c
+    print "\n\tKeyboardInterrupt detected, exiting gracefully!( at least trying :) )"
+    killall()
+    sys.exit(1)
